5ba474dd9521901afa4f523f38edd5a80debd275
Gustavo Cruz <gustavofrc8@gmail.com>
2021-09-22 22:35:50 -0300

    ⚡ base structure

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+  Container
+} from './style';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default Home;
